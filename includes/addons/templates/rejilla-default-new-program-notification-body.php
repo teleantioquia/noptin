ce6f179c6f7497305c8811ea2d53c046be5c2f9c
Jose Villalobos <jorovipe@gmail.com>
2020-12-29 17:34:40 -0500

    Updates to teleantioquia form

--- includes/addons/templates/rejilla-default-new-program-notification-body.php
+++ includes/addons/templates/rejilla-default-new-program-notification-body.php
@@ -1,17 +1,22 @@
-<h1>El programa <b>«[[program_name]]»</b> esta a punto de empezar</h1>
+<h1><b>«[[program_name]]»</b> empezará muy pronto.</h1>
 
-<p>Hola [[first_name]], espero te encuentres muy bien.</p>
+<p>Hola [[first_name]], te enviamos este correo porque que nos pediste que te notificáramos cuando <b>[[program_name]]</b> estuviera Al Aire.</p>
+
+<p>Nos emociona que estés conectado con nuestros contenidos y te invitamos a sintonizarnos ahora en:</p>
+<p><b>Une:</b></p>
+<p><b>Claro:</b></p>
+<p><b>Direct TV:</b></p>
 
 [[if_viewable_from_web]]
-<p>El programa, empieza en unos minutos, puedes mirarlo desde nuestra señal online o sintonizando el canal de Teleantioquia.</p>
+<p>O en nuestra señal vía internet:</p>
 <p>[[al_aire_button]]Mirar Online[[/al_aire_button]]</p>
-<p>Si el botón no funciona, intenta copiando y pegando el siguiente link en tu navegador:</p>
+<p>*Si el botón no funciona, intenta copiando y pegando el siguiente link en tu navegador:</p>
 <p>[[al_aire_link]]</p>
 [[/if_viewable_from_web]]
 
-[[if_no_viewable_from_web]]
-<p>El programa, empieza en unos minutos, este programa no esta disponible desde nuestra señal online, sin embargo, puedes verlo sintonizando nuestro canal de televisión.</p>
-[[/if_no_viewable_from_web]]
 
-<p>Con cariño,</p>
-<p>El equipo de teleantioquia</p>
+<p>Síguenos también en nuestras redes sociales:</p>
+<p>https://www.instagram.com/teleantioquia/</p>
+<p>https://www.facebook.com/CanalTeleantioquia</p>
+<p>https://www.youtube.com/user/CanalTeleantioquia</p>
+<p>https://twitter.com/Teleantioquia</p>
